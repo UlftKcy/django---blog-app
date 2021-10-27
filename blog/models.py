c6e26a9759ef87ff1eb770db11ccfb14469fd4d9
UlfetKacay <ukacay87@gmail.com>
2021-10-27 10:36:15 +0300

    models updated

--- blog/models.py
+++ blog/models.py
@@ -9,5 +9,5 @@ class User(AbstractUser):
 class Profile(models.Model):
     username = models.CharField(max_length=30)
     email = models.EmailField(max_length=154)
-    image = models.ImageField(upload_to='media/',blank=True,null=True,default="default.jpg")
+    image = models.ImageField(upload_to='media',blank=True,null=True)
     bio = models.CharField(max_length=200,blank=True)
--- blog/signals.py
+++ blog/signals.py
@@ -1,3 +1,21 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 
+from .models import Profile
+
+# create_profile is the receiver function which is run every time a user is created.
+# post_save is the signal that is sent at the end of the save method.
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    # updated user profile
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 
